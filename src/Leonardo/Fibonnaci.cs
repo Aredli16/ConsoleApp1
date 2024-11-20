6173d3a7b26c692d7e1dc7e558fda811658b3f43
corentin <xtarak@gmail.com>
2024-11-20 15:08:58 +0100

    Initial commit

---
+++ src/Leonardo/Fibonnaci.cs
@@ -0,0 +1,34 @@
+﻿namespace Leonardo;
+
+public record FibonacciResult(int Input, int Result);
+
+public static class Fibonnaci
+{
+    private static int Run(int i)
+    {
+        if (i <= 2)
+        {
+            return 1;
+        }
+
+        return Run(i - 1) + Run(i - 2);
+    }
+    
+    public static async Task<List<FibonacciResult>> RunAsync(string[] strings)
+    {
+        var tasks = new List<Task<FibonacciResult>>();
+        foreach (var input in strings)
+        {
+            var i = int.Parse(input);
+            tasks.Add(Task.Run(() => new FibonacciResult(i, Run(i))));
+        }
+
+        var results = new List<FibonacciResult>();
+        foreach (var task in tasks)
+        {
+            results.Add(await task);
+        }
+        
+        return results;
+    }
+}
---
+++ tests/Leonardo.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Leonardo;
+
+namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var strings = new string[] { "1", "2", "3", "4", "5" };
+        var results = Fibonnaci.RunAsync(strings).Result;
+        Assert.Equal(1, results[0].Result);
+        Assert.Equal(1, results[1].Result);
+        Assert.Equal(2, results[2].Result);
+        Assert.Equal(3, results[3].Result);
+        Assert.Equal(5, results[4].Result);
+    }
+}
